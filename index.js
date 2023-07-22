e78da5bbf96bf682e258168837d170952876b85f
Joell Malone <joellmalone000@gmail.com>
2023-07-22 15:44:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+
+    if(left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+document.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+});
+
+const DODGER = document.getElementById('dodger')
+
+function moveDodgerRight() {
+    const rightNumbers = DODGER.style.left.replace('px', '');
+    const right = parseInt(rightNumbers, 10);
+
+    if(right > 0) {
+        DODGER.style.left = `${right + 1}px`
+    }
+}
+document.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowRight') {
+        moveDodgerRight()
+    }
+})
